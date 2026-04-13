4676d419b0a5011d95b4928e313076c7899be17e
abilijn <abilijn@gmail.com>
2026-04-13 18:17:02 -0400

    commit 2

---
+++ classtest.R
@@ -0,0 +1,15 @@
+# my practice script
+print(2+4)
+
+zone = Sys.timezone()
+
+start = Sys.time()
+end = Sys.time()
+
+diff = end - start
+
+
+cat("You are in the", zone, "timezone")
+
+
+cat("You are in the", zone,"time zone. I took", diff,"seconds to run this code.")
